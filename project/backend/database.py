3b45ba7f62f74c8b1836a4efa0b6fe5be164ecde
hf4560 <urijpocepaev58090@gmail.com>
2025-02-26 23:45:07 +0300

    fix imports

--- project/backend/database.py
+++ project/backend/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://app_user:password@localhost:5432/volunteer_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://kp_user:1597530@localhost:5432/volunteer_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
