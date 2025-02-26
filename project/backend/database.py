135ab579d3aff9efe68c817338bfcfddf431a360
hf4560 <urijpocepaev58090@gmail.com>
2025-02-27 00:35:27 +0300

    MBA-21-Разработать-модуль-подключения-к-БД

--- project/backend/database.py
+++ project/backend/database.py
@@ -10,9 +10,20 @@ password = config.getValue('database', 'password')
 host = config.getValue('database', 'host')
 port = config.getValue('database', 'port')
 
-SQLALCHEMY_DATABASE_URL = f"{protocol}://{user}:{password}@{host}:{port}/volunteer_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+class DataBase(create_engine):
+    def __init__(self, database):
+        self.__SQLALCHEMY_DATABASE_URL = f"{protocol}://{user}:{password}@{host}:{port}/{database}"
+        super().__init__(self.__SQLALCHEMY_DATABASE_URL)
+        self.connect()
+        try:
+            with self.connect() as connection:
+                print("Database connection successful!")
+        except Exception as e:
+            print(f"Database connection failed: {e}")
+        self.__sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self)
+        self.__base = declarative_base()
+
+
 
